91abfbced49c52764404169e6708191b7257aeeb
Felix Octaniel <felixoctanieltel05@gmail.com>
2026-06-07 22:13:31 +0700

    update logo profile

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,12 +1,11 @@
 const NavbarLogo = () => {
   return (
-    <div>
-      <h1 className="text-white  text-2xl sm:hidden md:block">
-        Felix Octaniel 
-      </h1>
-      <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        𝓯
-      </h1>
+    <div className="flex items-center gap-6 cursor-pointer select-none">
+      <img
+        src="images/logo.png"
+        alt="Felix Octaniel Logo"
+        className="h-16 w-full object-contain"
+      />
     </div>
   );
 };
