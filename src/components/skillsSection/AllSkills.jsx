599dbf5af971f7f7b45aa483ea3f79e186adb78e
Felix Octaniel <felixoctanieltel05@gmail.com>
2026-06-10 20:58:57 +0700

    modify gap tools and collaboration

--- src/components/skillsSection/AllSkills.jsx
+++ src/components/skillsSection/AllSkills.jsx
@@ -75,7 +75,7 @@ const AllSkills = () => {
           </div>
 
           {/* Skills grid */}
-          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-3">
+          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-3 pb-3">
             {cat.skills.map((item, i) => (
               <motion.div
                 key={i}
