859731813a0e4ce02d9f48c8e3b7084fbfdcfb35
Felix Octaniel <felixoctanieltel05@gmail.com>
2026-06-10 21:11:41 +0700

    fix: logo

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,14 +1,14 @@
 const NavbarLogo = () => {
   return (
-    <div className="logo-fire-wrap h-16 w-auto cursor-pointer select-none">
-      <div className="flame-glow" />
-      <div className="flame-base" />
-      <div className="flame-mid" />
-      <div className="flame-tip" />
+    <div className="logo-fire-wrap  relative h-16 w-auto cursor-pointer select-none">
+      {/* <div className="flame-glow absolute inset-0 z-0" /> */}
+      {/* <div className="flame-base absolute inset-0 z-0" /> */}
+      {/* <div className="flame-mid absolute inset-0 z-0" /> */}
+      {/* <div className="flame-tip absolute inset-0 z-0" /> */}
       <img
         src="images/logo.png"
         alt="Felix Octaniel Logo"
-        className="h-16 w-auto object-contain"
+        className="relative z-10 h-16 w-auto object-contain"
       />
     </div>
   );
