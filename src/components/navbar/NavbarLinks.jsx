c496189b22b7815d6793186e83a2d2de3ee55f4c
Felix Octaniel <felixoctanieltel05@gmail.com>
2026-07-20 12:07:37 +0700

    fix: update background navbar by responsive

--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -13,7 +13,7 @@ const links = [
 
 const NavbarLinks = () => {
   return (
-    <ul className="flex lg:flex-row sm:flex-col gap-6 text-white font-body lg:relative sm:absolute sm:top-[120%] text-center left-[50%] -translate-x-[50%] lg:text-md sm:text-xl  backdrop-blur-lg lg:bg-transparent sm:w-full py-4">
+    <ul className="flex lg:flex-row sm:flex-col gap-6 text-white font-body lg:relative sm:absolute sm:top-[120%] text-center left-[50%] -translate-x-[50%] lg:translate-x-0 lg:left-auto lg:text-md sm:text-xl backdrop-blur-xl lg:backdrop-blur-none sm:bg-black/85 lg:bg-transparent sm:w-full sm:max-w-[calc(100vw-2rem)] lg:max-w-none py-5 sm:px-6 lg:px-0 sm:rounded-3xl lg:rounded-none sm:border sm:border-white/10 lg:border-0 sm:shadow-2xl sm:shadow-black/50 lg:shadow-none">
       {links.map((link, index) => {
         return (
           <li key={index} className="group">
